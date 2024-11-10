38f16c95e9dc50093bf8028b5d474d8b9d768ac7
Gulyás György (pH) <gyorgy.gulyas@prodhost.com>
2024-11-10 08:44:18 +0100

    WIP

---
+++ src/d3i/interpreter/__init__.py
@@ -0,0 +1,5 @@
+from .Elements import *
+from .ElementVisitor import *
+from .Parser import *
+
+Parser = Parser
--- src/tests/test_interpreter.py
+++ src/tests/test_interpreter.py
@@ -1,21 +1,23 @@
 import unittest
-import d3i.interpreter as d3i
+import d3i
 
 class TestInterpreter(unittest.TestCase):
 
     def test_empty_ok(self):
-        parser = d3i.Parser()
-        root:d3i = parser.ParseText( "")
+        parser = d3i.interpreter.Parser()
+        root = parser.ParseText( "")
         self.assertIsInstance( root, d3i)
         self.assertEqual(0, len(root.directives))
         self.assertEqual(0, len(root.domains))
     
     def test_directive_ok(self):
-        parser = d3i.Parser()
-        root:d3i = parser.ParseText( "import somedomain")
+        parser = d3i.interpreter.Parser()
+        root = parser.ParseText( "import somedomain")
         self.assertIsInstance( root, d3i)
         self.assertEqual(1, len(root.directives))
-        directives: d3i.directive = root.directives[0]
+        directive: d3i.interpreter.directive = root.directives[0]
+        self.assertEqual(directive.keyword, "import")
+        self.assertEqual(directive.value, "somedomain")
 
 if __name__ == "__main__":
     unittest.main()
