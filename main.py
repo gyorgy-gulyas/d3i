4646a2c4d77d1c7ea3fdcb1a24b319c7536a08c9
Gulyás György <gulyas.gyorgy@gmail.com>
2024-11-18 13:31:33 +0100

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import d3i
 import argparse
 import os
+import importlib.util
+from pathlib import Path
 
 arg_parser = argparse.ArgumentParser(
     description="This program processes d3i files and produces results according to the specified emitter.")
@@ -17,7 +19,7 @@ arg_parser.add_argument("-l",
                         default=[])
 arg_parser.add_argument("-aoe",
                         "--abort-on-error",
-                        help="when the any of the linter reports error, then no emitter called",
+                        help="when any file has a syntax erro, or any of the linter reports error, then no emitter called and the executing is aborted",
                         action="store_true")
 arg_parser.add_argument("-e",
                         "--emitter",
@@ -28,33 +30,66 @@ arg_parser.add_argument("-o",
                         "--output-dir",
                         help="output directory",
                         type=str,
-                        default=["./"])
+                        default="./")
+arg_parser.add_argument("-v",
+                        "--verbose",
+                        help="detailed output",
+                        action="store_true")
 
 args = arg_parser.parse_args()
 
 if (len(args.input) == 0):
     print("at least on input must be specified, use -h to see help.")
 
-
+#region process inputs
 engine = d3i.Engine()
 session = d3i.Session()
-
 for input in args.input:
     if os.path.exists(input) == False:
           exit(f"'{input}' file does not exist")
     else:
         session.AddSource(d3i.Source.CreateFromFile( input ))
+        if(args.verbose):
+            print(f"information: '{input}' file found, and added to sources")
 
+if(args.verbose):
+    print( f"information: {len(session.sources)} file will be processed")
 root = engine.Build(session)
-print(root)
 
-# parser = d3i.elements.Parser()
-# d3i = parser.ParseText( """
-# domain somedomain {
-#    context context_1 {
-#        valueObject Address {
-#        }
-#    }
-# }""")
-# parser.PrintTree()
-# print(d3i)
+if(session.HasErrror() == True):
+    if(args.verbose):
+        session.PrintErrors()
+    if(args.abort_on_error):
+        exit( "abort on error is enabled, process is aborted")
+else:
+    if(args.verbose):
+        print( f"information: no error found in build")
+#endregion process inputs
+
+#region linting
+for linter_file in args.linter:
+    spec = importlib.util.spec_from_file_location(Path(linter_file).stem, linter_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    module.DoLint( session )
+
+if(session.HasErrror() == True):
+    if(args.verbose):
+        session.PrintErrors()
+    if(args.abort_on_error):
+        exit( "abort on error is enabled, prcess is aborted")
+#endregion process inputs
+
+#region emitting
+for emitter_file in args.emitter:
+    spec = importlib.util.spec_from_file_location(Path(emitter_file).stem, emitter_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    print(module.DoEmit( session, args.output_dir ))
+
+if(session.HasErrror() == True):
+    if(args.verbose):
+        session.PrintErrors()
+    if(args.abort_on_error):
+        exit( "abort on error is enabled, process is aborted")
+#endregion process inputs
